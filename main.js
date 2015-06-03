2bcbbee228df3976034f0becb9ca7cd7ce3d5845
cody-sitegoals <cody@Amandas-iMac.local>
2015-06-03 13:47:50 -0500

    1

---
+++ main.js
@@ -0,0 +1,45 @@
+/**
+ * InfoWars.com - Sitegoals
+ *
+ */
+jQuery(document).ready(function() {
+	initMenuHover();
+	inlineSocial();
+});
+jQuery(window).resize(function(){
+	//about a week ago
+});
+
+function initMenuHover(){
+	//keeps top menu item is highlighted when hovering over child items. 
+	jQuery('#menu-navigation-header ul li').mouseenter(function(){
+		jQuery(this).parents('li').addClass('hover');
+	});
+	jQuery('#menu-navigation-header ul li').mouseleave(function(){
+		jQuery(this).parents('li').removeClass('hover');
+	});
+}
+
+
+jQuery(function(jQuery) {
+
+	jQuery( document ).ready(function() {
+
+		jQuery("a.single_image").fancybox({
+			'transitionIn'	:	'elastic',
+			'transitionOut'	:	'elastic',
+			'speedIn'		:	600, 
+			'speedOut'		:	200, 
+			'overlayShow'	:	false
+		});
+
+		jQuery("img").addClass("img-responsive");
+
+		
+	});
+});
+
+function inlineSocial(){
+	jQuery('.synved-social-container-share').addClass('inline-social');
+	console.log('test');
+}
